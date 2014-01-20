c2ecd5b27c76259b480d411bf781d50f11531163
Mateusz Fedoryszak <matfed@icm.edu.pl>
2014-01-20 11:22:25 +0100

    solr plugged in

--- src/main/java/pl/edu/icm/coansys/webdemo/service/CitationMatchingService.java
+++ src/main/java/pl/edu/icm/coansys/webdemo/service/CitationMatchingService.java
@@ -6,7 +6,10 @@
 package pl.edu.icm.coansys.webdemo.service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import org.apache.commons.lang.StringUtils;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -21,15 +24,20 @@ import pl.edu.icm.coansys.webdemo.data.json.DocumentMetadata;
 import pl.edu.icm.coansys.webdemo.data.json.ExtractedMetadata;
 import pl.edu.icm.coansys.webdemo.data.json.MatchedDocument;
 import pl.edu.icm.coansys.webdemo.data.json.ResultEntry;
+import scala.collection.JavaConverters;
 
+import static pl.edu.icm.coansys.citations.util.misc.digitsNormaliseTokenise;
+import static pl.edu.icm.coansys.citations.util.misc.approximateYear;
+import static pl.edu.icm.coansys.citations.util.misc.lettersNormaliseTokenise;
+import scala.Function1;
 /**
  *
  * @author matfed
  */
 @Component
 public class CitationMatchingService {
-    private static final String SOLR_URL = "http://localhost:8983/solr";
-//    private SolrServer solrServer = new HttpSolrServer(SOLR_URL);
+    private static final String SOLR_URL = "http://localhost:8983/solr/citation-matching/";
+    private SolrServer solrServer = new HttpSolrServer(SOLR_URL);
     
     public ResultEntry matchCitation(String citationText) throws AnalysisException, SolrServerException {
         CRFBibReferenceParser parser = new CRFBibReferenceParser(
@@ -51,15 +59,30 @@ public class CitationMatchingService {
     }
     
     public List<DocumentMetadata> getHeuristicallyMatching(MatchableEntity entity) throws SolrServerException {
-//        SolrQuery query = new SolrQuery();
-//        query.setQuery( "*:*" );
-//        QueryResponse rsp = solrServer.query( query );
-//        rsp.getBeans(DocumentMetadata.class)
-        List<DocumentMetadata> result = new ArrayList<DocumentMetadata>();
+        String text = entity.rawText().get();
         
-        result.add(new DocumentMetadata("doc1", "10.1000/182", "Nicolaus Copernicus", "1543", "De revolutionibus orbium coelestium", null, null));
-        result.add(new DocumentMetadata("doc123", null, null, null, null, null, null));
+        Set<String> years = new HashSet<String>();
+        for(String year : JavaConverters.asJavaListConverter(digitsNormaliseTokenise(text)).asJava()) {
+            for (String approx : JavaConverters.asJavaListConverter(approximateYear(year)).asJava()) {
+                if (approx.length() == 4) {
+                    years.add(approx);
+                }
+            }
+        }
         
-        return result;
+        String yearStr = StringUtils.join(years, " ");
+        
+        String authorStr = StringUtils.join(JavaConverters.asJavaSetConverter(lettersNormaliseTokenise(text).toSet()).asJava(), " ");
+                
+        String queryStr = "author_idxd:(" + authorStr + ") AND year_idxd:(" + yearStr + ")";
+        SolrQuery query = new SolrQuery(queryStr);        
+        QueryResponse rsp = solrServer.query(query);
+        return rsp.getBeans(DocumentMetadata.class);
+//        List<DocumentMetadata> result = new ArrayList<DocumentMetadata>();
+//        
+//        result.add(new DocumentMetadata("doc1", "10.1000/182", "Nicolaus Copernicus", "1543", "De revolutionibus orbium coelestium", null, null));
+//        result.add(new DocumentMetadata("doc123", null, null, null, null, null, null));
+//        
+//        return result;
     }
 }
