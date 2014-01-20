231653e5ba0150b39312c5ba00615aa01a3011c9
pdendek <pdendek@icm.edu.pl>
2014-01-20 16:08:47 +0100

    Spring configuration for CoansysController.documentSimarityService: changes in the structure of the class, applicationContext.xml; added values for connection with Postgres

--- src/main/java/pl/edu/icm/coansys/webdemo/service/DocumentSimarityService.java
+++ src/main/java/pl/edu/icm/coansys/webdemo/service/DocumentSimarityService.java
@@ -8,8 +8,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Comparator;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import pl.edu.icm.coansys.commons.java.StackTraceExtractor;
@@ -21,42 +25,48 @@ public class DocumentSimarityService {
 	/**
 	 * 
 	 * @author pdendek
-	 *
-	 * The class contains information about seconds document doi and its similarity to the first one.
+	 * 
+	 *         The class contains information about seconds document doi and its
+	 *         similarity to the first one.
 	 */
-	public static class SecondDocument{
+	public static class SecondDocument {
 		public String doib;
 		public float sim;
-		
-		public SecondDocument(String doib,float sim){
+
+		public SecondDocument(String doib, float sim) {
 			this.doib = doib;
 			this.sim = sim;
 		}
-		
-		final static Comparator<SecondDocument> asc = new Comparator<SecondDocument>(){
+
+		final static Comparator<SecondDocument> asc = new Comparator<SecondDocument>() {
 			@Override
 			public int compare(SecondDocument o1, SecondDocument o2) {
-				return (o1.sim-o2.sim > 0 ? 1 : -1);
-			}			
+				return (o1.sim - o2.sim > 0 ? 1 : -1);
+			}
 		};
-		
-		final static Comparator<SecondDocument> desc = new Comparator<SecondDocument>(){
+
+		final static Comparator<SecondDocument> desc = new Comparator<SecondDocument>() {
 			@Override
 			public int compare(SecondDocument o1, SecondDocument o2) {
-				return -(o1.sim-o2.sim > 0 ? 1 : -1);
+				return -(o1.sim - o2.sim > 0 ? 1 : -1);
 			}
 		};
 	}
-	
-	static Logger logger = LoggerFactory.getLogger(DocumentSimarityService.class);
-	
+
+	static Logger logger = LoggerFactory
+			.getLogger(DocumentSimarityService.class);
+
+	@Value("${docsim.postgres.dbhost}")
 	String dbhost = null;
+	@Value("${docsim.postgres.dbname}")
 	String dbname = null;
+	@Value("${docsim.postgres.dbuser}")
 	String dbuser = null;
+	@Value("${docsim.postgres.dbpasswd}")
 	String dbpasswd = null;
 
 	Connection con = null;
-	
+
 	public String getDbhost() {
 		return dbhost;
 	}
@@ -97,53 +107,72 @@ public class DocumentSimarityService {
 		this.con = con;
 	}
 
-	public DocumentSimarityService() {
-
+	public DocumentSimarityService(String dbhost, String dbname, 
+			String dbuser, String dbpasswd) {
+		this.dbhost = dbhost;
+		this.dbname = dbname;
+		this.dbuser = dbuser;
+		this.dbpasswd = dbpasswd;
 	}
 
-	public DocumentSimarityService init() throws SQLException {
+	public DocumentSimarityService() {
 		this.dbhost = "localhost";
 		this.dbname = "docsimtestdb";
 		this.dbuser = "postgres";
 		this.dbpasswd = "postgres";
+	}
+
+	@PostConstruct
+	public DocumentSimarityService init() throws SQLException {
 		getConnection();
 		return this;
 	}
 
+	@PreDestroy
+	public void tearDown() {
+		try {
+			if (con != null) {
+				con.close();
+			}
+
+		} catch (SQLException ex) {
+			logger.error(StackTraceExtractor.getStackTrace(ex));
+		}
+	}
+	
 	synchronized public void getConnection() throws SQLException {
 		if (con == null) {
 			String url = "jdbc:postgresql://" + dbhost + "/" + dbname;
-			String user = dbuser;
-			String password = dbpasswd;
-			con = DriverManager.getConnection(url, user, password);
+			con = DriverManager.getConnection(url, dbuser, dbpasswd);
 		}
 	}
 
-	/**
+/**
 	 * 
 	 * @param doi 
 	 * @param maxNum number of desired similar elements
 	 * @return returns an array of objects of class {@link SecondDocument), sorted in descending order  
 	 * @throws SQLException
 	 */
-	public ArrayList<SecondDocument> getSims(String doi, int maxNum) throws SQLException {
+	public ArrayList<SecondDocument> getSims(String doi, int maxNum)
+			throws SQLException {
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			st = con.prepareStatement(DocumentSimilarityConstants.QUERY_DOCUMENT_SIMS);
 			st.setString(1, doi);
 			st.setInt(2, maxNum);
 			rs = st.executeQuery();
-			
+
 			ArrayList<SecondDocument> ret = new ArrayList<SecondDocument>();
 			String doib = null;
 			Float sim = null;
-			
+
 			while (rs.next()) {
 				doib = rs.getString("doib");
 				sim = rs.getFloat("sim");
-				ret.add(new SecondDocument(doib,sim));
+				ret.add(new SecondDocument(doib, sim));
 			}
 			rs.close();
 			st.close();
@@ -164,23 +193,23 @@ public class DocumentSimarityService {
 			}
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param doi
-	 * @return returns a String array of a name of authors 
+	 * @return returns a String array of a name of authors
 	 * @throws SQLException
 	 */
 	public ArrayList<String> getAuthors(String doi) throws SQLException {
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			st = con.prepareStatement(DocumentSimilarityConstants.QUERY_DOCUMENT_AUTHORS);
 			st.setString(1, doi);
 			rs = st.executeQuery();
-			
-			ArrayList<String> names = new ArrayList<String>(); 
+
+			ArrayList<String> names = new ArrayList<String>();
 			while (rs.next()) {
 				names.add(rs.getString("name"));
 			}
@@ -202,7 +231,7 @@ public class DocumentSimarityService {
 			}
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param doi
@@ -212,22 +241,22 @@ public class DocumentSimarityService {
 	public String[] getDocument(String doi) throws SQLException {
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			st = con.prepareStatement(DocumentSimilarityConstants.QUERY_DOCUMENT_MAIN_DATA);
 			st.setString(1, doi);
 			rs = st.executeQuery();
-			
-			String year = null,title = null;
-			
+
+			String year = null, title = null;
+
 			if (rs.next()) {
 				year = rs.getString("year");
 				title = rs.getString("title");
 			}
 			rs.close();
 			st.close();
-			
-			return new String[]{doi,year,title};
+
+			return new String[] { doi, year, title };
 		} catch (SQLException ex) {
 			logger.error(StackTraceExtractor.getStackTrace(ex));
 			return null;
@@ -242,18 +271,6 @@ public class DocumentSimarityService {
 			} catch (SQLException ex) {
 				logger.error(StackTraceExtractor.getStackTrace(ex));
 			}
-		}
-	}
-	
-	public void tearDown(){
-
-		try {
-			if (con != null) {
-				con.close();
-			}
-
-		} catch (SQLException ex) {
-			logger.error(StackTraceExtractor.getStackTrace(ex));
 		}
 	}
 }
